8adb5b514b6cade103c8355a54b9b90905848d02
ujvaridominik <70593061+ujvaridominik@users.noreply.github.com>
2020-09-23 15:35:48 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2020._0922ora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
